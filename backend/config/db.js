ad35462b4577305ce1463fe9fbd876bb8fdc89b0
Akash kumar <kakash2023@gift.edu.in>
2026-07-07 16:23:15 +0530

    Remove node_modules from Git

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,31 @@
+// import mongoose from "mongoose";
+
+// const connectDb =async ()=>{
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URL)
+//         console.log("db connected")
+//     } catch (error) {
+//         console.log("db error")
+//     }
+// }
+
+// export default connectDb
+
+
+
+
 import mongoose from "mongoose";
 
-const connectDb =async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-        console.log("db error")
-    }
-}
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("✅ MongoDB Connected");
+    console.log(conn.connection.host);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:");
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-export default connectDb
+export default connectDb;
